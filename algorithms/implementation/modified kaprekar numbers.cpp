987eae4f0eef63e3925e81e19f03a4e272199fdc
sihasuom <sihasrox@gmail.com>
2024-06-12 14:22:41 +0530

    Completed 'Modified Kaprekar Numbers' challenge

---
+++ algorithms/implementation/modified kaprekar numbers.cpp
@@ -0,0 +1,178 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'kaprekarNumbers' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER p
+ *  2. INTEGER q
+ */
+
+string multiply(string num1, string num2)
+{
+    int n1 = num1.size();
+    int n2 = num2.size();
+    if (n1 == 0 || n2 == 0)
+        return "0";
+
+    // will keep the result number in vector
+    // in reverse order
+    vector<int> result(n1 + n2, 0);
+
+    // Below two indexes are used to find positions
+    // in result.
+    int i_n1 = 0;
+    int i_n2 = 0;
+
+    // Go from right to left in num1
+    for (int i = n1 - 1; i >= 0; i--)
+    {
+        int carry = 0;
+        int n1 = num1[i] - '0';
+
+        // To shift position to left after every
+        // multiplication of a digit in num2
+        i_n2 = 0;
+
+        // Go from right to left in num2
+        for (int j = n2 - 1; j >= 0; j--)
+        {
+            // Take current digit of second number
+            int n2 = num2[j] - '0';
+
+            // Multiply with current digit of first number
+            // and add result to previously stored result
+            // at current position.
+            int sum = n1 * n2 + result[i_n1 + i_n2] + carry;
+
+            // Carry for next iteration
+            carry = sum / 10;
+
+            // Store result
+            result[i_n1 + i_n2] = sum % 10;
+
+            i_n2++;
+        }
+
+        // store carry in next cell
+        if (carry > 0)
+            result[i_n1 + i_n2] += carry;
+
+        // To shift position to left after every
+        // multiplication of a digit in num1.
+        i_n1++;
+    }
+
+    // ignore '0's from the right
+    int i = result.size() - 1;
+    while (i >= 0 && result[i] == 0)
+        i--;
+
+    // If all were '0's - means either both or
+    // one of num1 or num2 were '0'
+    if (i == -1)
+        return "0";
+
+    // generate the result string
+    string s = "";
+
+    while (i >= 0)
+        s += std::to_string(result[i--]);
+
+    return s;
+}
+
+bool isKaprekar(int n)
+{
+    if (n == 1)
+        return true;
+
+    int d = to_string(n).size();
+
+    string n_sq_as_str = multiply(to_string(n), to_string(n));
+
+    int sq_len = (int)n_sq_as_str.size();
+    if (sq_len < 2)
+        return false;
+
+    string right = "";
+    string left = "";
+
+    for (int i = 0; i < sq_len; i++)
+    {
+        if (i >= sq_len - d)
+            right += n_sq_as_str[i];
+
+        else
+            left += n_sq_as_str[i];
+    }
+
+    int sum = stoi(left) + stoi(right);
+
+    if (sum == n)
+        return true;
+
+    return false;
+}
+
+void kaprekarNumbers(int p, int q)
+{
+    int count = 0;
+    for (int i = p; i < q + 1; i++)
+    {
+        if (isKaprekar(i))
+        {
+            cout << i << " ";
+            count++;
+        }
+    }
+
+    if (not count)
+    {
+        cout << "INVALID RANGE" << endl;
+    }
+}
+
+int main()
+{
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    kaprekarNumbers(p, q);
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end());
+
+    return s;
+}
